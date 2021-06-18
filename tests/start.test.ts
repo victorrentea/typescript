cd6494a39612a7b7137297f5c13a660bcbf6d145
Victor Rentea <victorrentea@gmail.com>
2021-06-18 06:49:40 +0300

    +2

--- tests/start.test.ts
+++ tests/start.test.ts
@@ -1,11 +1,11 @@
 import {expect} from 'chai';
 import {describe, it} from 'mocha';
 import {GameRunner} from '../src/game-runner';
-import {calculateOrderPrice, X} from "../src/SplitPhase2";
+import {calculateOrderPrice} from "../src/SplitPhase2";
+
 
 describe('The test environment', () => {
     it('should pass', () => {
-        let x = new X();
         console.log(calculateOrderPrice("Chair-CHR 4", new Map([["CHR", 5]])));
         expect(true).to.be.true;
     });
