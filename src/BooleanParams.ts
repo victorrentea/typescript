4f4508e04ee76e1143d08d2f41326b01a2d51110
Victor Rentea <victorrentea@gmail.com>
2023-12-06 12:24:36 +0200

    m-am panicat

--- src/BooleanParams.ts
+++ src/BooleanParams.ts
@@ -6,19 +6,26 @@ bigUglyMethod(4, 2);
 bigUglyMethod(5, 1);
 
 // TODO From my use-case #323, I call it too, to do more within:
-bigUglyMethod(2, 1, true);
+bigUglyMethod323(2, 1);
 
 
-function bigUglyMethod(b: number, a: number, boulean: boolean = false) {
+// Single Responsibility Principle violation
+function bigUglyMethod(b: number, a: number) {
+  donkey();
+  cow();
+}
+function bigUglyMethod323(b: number, a: number) {
+  donkey();
+  console.log("Some stuff only for CR323 4 " + a);
+  cow();
+}
+function cow() {
+  console.log("More Cow Logic " + b);
+  console.log("More Cow Logic2 " + b);
+}
+function donkey() {
   console.log("Donkey Logic 1 " + a + " and " + b);
   console.log("Donkey Logic 2 " + a);
   console.log("Donkey Logic 3 " + a);
-
-  if (boulean) {
-    console.log("Some stuff only for CR323 4 " + a);
-  }
-
-  console.log("More Cow Logic " + b);
-  console.log("More Cow Logic2 " + b);
 }
 
