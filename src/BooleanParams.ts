31c2172b6d5df88be24adaca2f4b1c3ca9b22649
Victor Rentea <victorrentea@gmail.com>
2024-03-14 17:34:12 +0200

    I'm safe. I'll keep my job

--- src/BooleanParams.ts
+++ src/BooleanParams.ts
@@ -1,28 +1,27 @@
-// existing callers
-bigUglyMethod(1, 5);
-bigUglyMethod(2, 4);
-bigUglyMethod(3, 3);
-bigUglyMethod(4, 2);
-bigUglyMethod(5, 1);
-
-// TODO From my use-case #323, I call it too, to do more within:
-bigUglyMethod(2, 1, (productCount: number) => {
-    console.log("Some stuff only for CR323 4 " + productCount);
-});
-
 
 // 100% safe changes: rename of a param, local const/let; extract a function
-function bigUglyMethod(discountCode: number,
-                       productCount: number,
-                       f: (productCount: number) => void = (_: number) => {}) {
+const bigUglyMethod = (discountCode: number) => (productCount: number) => (f: (productCount: number) => void = (_)=>{}) => {
     console.log("Donkey Logic 1 " + productCount + " and " + discountCode);
     console.log("Donkey Logic 2 " + productCount);
     console.log("Donkey Logic 3 " + productCount);
 
     // f && f(productCount); // PR reject: don't! Use if(f) instead
-    f(productCount); // overengineered
+    // f(productCount); // overengineered
+    f(productCount);
 
     console.log("More Cow Logic " + discountCode);
     console.log("More Cow Logic2 " + discountCode);
-    const x = 1;
-}
+};
+
+// existing callers
+const fff = bigUglyMethod(1);
+fff(5)();
+bigUglyMethod(2)(4)();
+bigUglyMethod(3)(3)();
+bigUglyMethod(4)(2)();
+bigUglyMethod(5)(1)();
+
+// TODO From my use-case #323, I call it too, to do more within:
+bigUglyMethod(2)(1)(productCount =>
+    console.log("Some stuff only for CR323 4 " + productCount)
+);
