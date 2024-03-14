f7169f9acd4512712227aa2c19c6e2ea9fec1e7b
Victor Rentea <victorrentea@gmail.com>
2024-03-14 12:34:18 +0200

    we created a new type that simplified a signature (intersect) and also allowed to reduce the no of attributes of other structures

--- src/ValueObjects.ts
+++ src/ValueObjects.ts
@@ -6,55 +6,52 @@ import {expect} from "chai";
 //    return MathUtil.intervalsIntersect(model.startYear, model.endYear, criteria.startYear, criteria.endYear);
 // }
 function filterCarModels(criteria: CarSearchCriteria, models: CarModel[]): CarModel[] {
-  // slightly more complicated as I have a variable keeping a function
-  const filterByYears = (model:CarModel) => MathUtil.intervalsIntersect(
-      model.startYear,
-      model.endYear,
-      criteria.startYear,
-      criteria.endYear);
-  const results = models.filter(filterByYears)
-  console.log("More filtering logic");
-  return results;
+    // slightly more complicated as I have a variable keeping a function
+    const results = models.filter(model => MathUtil.intervalsIntersect(
+        model.yearInterval,criteria.yearInterval))
+    console.log("More filtering logic");
+    return results;
 }
 
 function applyCapacityFilter() {
-  console.log(MathUtil.intervalsIntersect(1000, 1600, 1250, 2000));
+    console.log(MathUtil.intervalsIntersect({start: 1000, end: 1600}, {start: 1250, end: 2000}));
 }
 
+type Interval = { start: number, end: number }
+
 class MathUtil {
-  // namespaced, but more to write than a global function
-  static intervalsIntersect(start1: number, end1: number, start2: number, end2: number): boolean {
-    return start1 <= end2 && start2 <= end1;
-  }
+    // namespaced, but more to write than a global function
+    static intervalsIntersect(interval1: Interval, interval2: Interval): boolean {
+        return interval1.start <= interval2.end && interval2.start <= interval1.end;
+    }
 }
 
 
 class CarSearchCriteria {
-  constructor(public readonly startYear: number,
-              public readonly endYear: number,
-              public readonly make: string) {
-    if (startYear > endYear) throw new Error("start larger than end");
-  }
+    constructor(public readonly yearInterval: Interval,
+                public readonly make: string) {
+        if (yearInterval.start > yearInterval.end) {
+            throw new Error("start larger than end");
+        }
+    }
 }
 
 class CarModel {
-  constructor(public readonly make: string,
-              public readonly model: string,
-              public readonly startYear: number,
-              public readonly endYear: number) {
-    if (startYear > endYear) {
-      throw new Error("start larger than end");
+    constructor(public readonly make: string,
+                public readonly model: string,
+                public readonly yearInterval: Interval //!WARNING: you can't do this on DTOs from server
+                ) {
+        if (yearInterval.start > yearInterval.end) {
+            throw new Error("start larger than end");
+        }
     }
-  }
-
-
 }
 
 it('should filter car models', () => {
-  const criteria = new CarSearchCriteria(2014, 2018, "Ford");
-  const fordFocusMk2 = new CarModel("Ford", "Focus", 2012, 2016);
-  const models = filterCarModels(criteria, [fordFocusMk2]);
-  console.log(models);
-  expect(models).to.contain(fordFocusMk2);
+    const criteria = new CarSearchCriteria({start:2014, end:2018}, "Ford");
+    const fordFocusMk2 = new CarModel("Ford", "Focus", {start:2012, end:2016});
+    const models = filterCarModels(criteria, [fordFocusMk2]);
+    console.log(models);
+    expect(models).to.contain(fordFocusMk2);
 });
 
