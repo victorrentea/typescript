d5017f76b7412f3f0a8b29f1c58ecd65376d2386
Victor Rentea <victorrentea@gmail.com>
2024-03-14 11:52:28 +0200

    destructuring a type made out from the parameters of my function

--- src/Parameters.ts
+++ src/Parameters.ts
@@ -10,18 +10,20 @@
 
 import {it} from "mocha";
 import {expect} from "chai";
-
-export function createMenu(title: string, body?: string, cancellable?: boolean) {
-    cancellable = cancellable ?? false;
-    body = body ?? "N/A";
+type CreateMenuConfig = {
+    title: string,
+    body?: string,
+    cancellable?: boolean
+}
+export function createMenu({title, body="N/A", cancellable=false}: CreateMenuConfig) {
     console.log("Inside: ", title, body, cancellable);
 }
 
 
 it('parameters', () => {
-    createMenu("Bar", "Foo", true);
-    createMenu("Bar", "Foo");
-    createMenu("Bar");
+    createMenu({title: "Bar", body: "Foo", cancellable: true});
+    createMenu({title: "Bar", body: "Foo"});
+    createMenu({title: "Bar"});
     expect(true).to.be.true;
 });
 
