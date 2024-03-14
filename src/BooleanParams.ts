9842f74f3b4d1891ef1dd5ecbbd05e3181080013
Victor Rentea <victorrentea@gmail.com>
2024-03-14 11:41:15 +0200

    I went for smaller, focused methods. Following SRP = a function should do ONE THING.

--- src/BooleanParams.ts
+++ src/BooleanParams.ts
@@ -6,20 +6,39 @@ bigUglyMethod(4, 2);
 bigUglyMethod(5, 1);
 
 // TODO From my use-case #323, I call it too, to do more within:
-bigUglyMethod(2, 1, true);
+bigUglyMethod323(2, 1);
 
 
 // 100% safe changes: rename of a param, local const/let; extract a function
-function bigUglyMethod(discountCode: number, productCount: number, boulean: boolean = false) {
-  console.log("Donkey Logic 1 " + productCount + " and " + discountCode);
-  console.log("Donkey Logic 2 " + productCount);
-  console.log("Donkey Logic 3 " + productCount);
 
-  if (boulean) {
+function bigUglyMethod(discountCode: number, productCount: number) {
+    // default params: + for options; -not if the param conditions a lot of logic inside; - not too many
+    donkey(productCount, discountCode);
+    cow(discountCode);
+}
+
+function bigUglyMethod323(discountCode: number, productCount: number) {
+    donkey(productCount, discountCode);
+
     console.log("Some stuff only for CR323 4 " + productCount);
-  }
+    console.log("Some stuff only for CR323 4 " + productCount);
+    console.log("Some stuff only for CR323 4 " + productCount);
+    console.log("Some stuff only for CR323 4 " + productCount);
 
-  console.log("More Cow Logic " + discountCode);
-  console.log("More Cow Logic2 " + discountCode);
-  const x = 1;
+    cow(discountCode);
+}
+
+function donkey(productCount: number, discountCode: number) {
+    console.log("Donkey Logic 1 " + productCount + " and " + discountCode);
+    console.log("Donkey Logic 2 " + productCount);
+    console.log("Donkey Logic 3 " + productCount);
+    console.log("Donkey Logic 1 " + productCount + " and " + discountCode);
+    console.log("Donkey Logic 2 " + productCount);
+}
+function cow( discountCode: number) {
+    console.log("More Cow Logic " + discountCode);
+    console.log("More Cow Logic2 " + discountCode);
+    console.log("More Cow Logic " + discountCode);
+    console.log("More Cow Logic2 " + discountCode);
+    console.log("More Cow Logic2 " + discountCode);
 }
