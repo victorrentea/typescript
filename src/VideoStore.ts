88254aa9cd295e2892855dc545be4f5f2bea6439
Aleksandar Milosavljević <alemilosav@gmail.com>
2024-03-15 14:19:19 +0100

    first fix

--- src/VideoStore.ts
+++ src/VideoStore.ts
@@ -1,66 +1,80 @@
 export class Movie {
   public title: string;
   public priceCode: number;
+
+  constructor(title: string, priceCode: number) {
+    this.title = title;
+    this.priceCode = priceCode;
+  }
 }
 
-export const MOVIE_CATEGORY = {
-  CHILDRENS: 2,
-  REGULAR: 0,
-  NEW_RELEASE: 1
+export enum MOVIE_CATEGORY {
+  CHILDRENS = 2,
+  REGULAR = 0,
+  NEW_RELEASE = 1
 };
+
+
+class Rental {
+  public movie: Movie;
+  public daysRented: number;
+
+  constructor(movie: Movie, daysRented: number) {
+    this.movie = movie;
+    this.daysRented = daysRented;
+  }
+
+  get price() {
+    let thisAmount = 0;
+    switch (this.movie.priceCode) {
+      case MOVIE_CATEGORY.REGULAR:
+        thisAmount += 2;
+        if (this.daysRented > 2)
+          thisAmount += (this.daysRented - 2) * 1.5;
+        break;
+      case MOVIE_CATEGORY.NEW_RELEASE:
+        thisAmount += this.daysRented * 3;
+        break;
+      case MOVIE_CATEGORY.CHILDRENS:
+        thisAmount += 1.5;
+        if (this.daysRented > 3)
+          thisAmount += (this.daysRented - 3) * 1.5;
+        break;
+    }
+    return thisAmount;
+  }
+}
 
 
 export class Customer {
   private name: string;
-  private rentals: any[] = [];
+  private rentals: Rental[] = [];
+  private frequentRenterPoints: number = 0;
 
   constructor(name: string) {
     this.name = name;
   }
 
-  public addRental(m: Movie, d: number) {
-    this.rentals.push({d: d, m: m});
+  public addRental(movie: Movie, daysRented: number) {
+    this.rentals.push(new Rental(movie, daysRented));
+    this.frequentRenterPoints++;
+    if (movie.priceCode != null &&
+      (movie.priceCode == MOVIE_CATEGORY.NEW_RELEASE)
+      && daysRented > 1)
+      this.frequentRenterPoints++;
   }
 
   public statement(): string {
-    let totalAmount: number = 0;
-    let frequentRenterPoints = 0;
 
-    let result = "Rental Record for " + this.name + "\n";
-    for (const r of this.rentals) {
-      let each = r.m;
-      let thisAmount = 0;
-      let dr = r.d;
-      // determine amounts for each line
-      switch (each.priceCode) {
-        case MOVIE_CATEGORY.REGULAR:
-          thisAmount += 2;
-          if (dr > 2)
-            thisAmount += (dr - 2) * 1.5;
-          break;
-        case MOVIE_CATEGORY.NEW_RELEASE:
-          thisAmount += dr * 3;
-          break;
-        case MOVIE_CATEGORY.CHILDRENS:
-          thisAmount += 1.5;
-          if (dr > 3)
-            thisAmount += (dr - 3) * 1.5;
-          break;
-      }
-      // add frequent renter points
-      frequentRenterPoints++;
-      // add bonus for a two day new release rental
-      if (each.priceCode != null &&
-          (each.priceCode == MOVIE_CATEGORY.NEW_RELEASE)
-          && dr > 1)
-        frequentRenterPoints++;
-      // show figures line for this rental
-      result += "\t" + each.title + "\t" + thisAmount.toFixed(1) + "\n";
-      totalAmount += thisAmount;
-    }
-    // add footer lines
-    result += "Amount owed is " + totalAmount.toFixed(1) + "\n";
-    result += "You earned " + frequentRenterPoints + " frequent renter points";
-    return result;
+    const totalRentalsInfoLine = "Rental Record for " + this.name + "\n";
+
+    const individualRentalInfos = this.rentals.map((rental) => { return `\t${rental.movie.title}\t${rental.price.toFixed(1)}\n` });
+
+    const totalAmount = this.rentals.reduce((acc, rental) => acc + rental.price, 0);
+
+    const footerLine = "Amount owed is " + totalAmount.toFixed(1) + "\n";
+    const eaarnedLine = "You earned " + this.frequentRenterPoints + " frequent renter points";
+    return `${totalRentalsInfoLine}${individualRentalInfos.join('')}${footerLine}${eaarnedLine}`;
+
   }
 }
