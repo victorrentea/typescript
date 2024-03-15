4cf2c60d01884479e1f64354c2d6b96bd9d3106a
unknown <nikolajoksimovic95@gmail.com>
2024-03-15 14:22:18 +0100

    changes

--- src/VideoStore.test.ts
+++ src/VideoStore.test.ts
@@ -1,23 +1,31 @@
-import {expect} from 'chai';
-import {describe, it} from 'mocha';
-import {Customer, MOVIE_CATEGORY} from "./VideoStore";
+import { expect } from "chai";
+import { describe, it } from "mocha";
+import { Customer, MOVIE_CATEGORY } from "./VideoStore";
 
-
-describe('Videostore', () => {
-    it('Characterization Test ', function () {
-
+describe("Videostore", () => {
+  it("Characterization Test ", function () {
     const customer = new Customer("John Doe");
-    customer.addRental({title:"Star Wars", priceCode: MOVIE_CATEGORY.NEW_RELEASE}, 6);
-    customer.addRental({title:"Sofia", priceCode: MOVIE_CATEGORY.CHILDRENS}, 7);
-    customer.addRental({title:"Inception", priceCode: MOVIE_CATEGORY.REGULAR}, 5);
+    customer.addRental(
+      { title: "Star Wars", priceCode: MOVIE_CATEGORY.NEW_RELEASE },
+      6,
+    );
+    customer.addRental(
+      { title: "Sofia", priceCode: MOVIE_CATEGORY.CHILDREN },
+      7,
+    );
+    customer.addRental(
+      { title: "Inception", priceCode: MOVIE_CATEGORY.REGULAR },
+      5,
+    );
 
-    const expected = "Rental Record for John Doe\n"
-      + "	Star Wars	18.0\n"
-      + "	Sofia	7.5\n"
-      + "	Inception	6.5\n"
-      + "Amount owed is 32.0\n"
-      + "You earned 4 frequent renter points";
+    const expected =
+      "Rental Record for John Doe\n" +
+      "	Star Wars	18.0\n" +
+      "	Sofia	7.5\n" +
+      "	Inception	6.5\n" +
+      "Amount owed is 32.0\n" +
+      "You earned 4 frequent renter points";
 
-      expect(customer.statement()).to.equal(expected);
+    expect(customer.statement()).to.equal(expected);
   });
 });
--- src/VideoStore.ts
+++ src/VideoStore.ts
@@ -1,25 +1,29 @@
 export class Movie {
   public title: string;
   public priceCode: number;
+  constructor(title: string, priceCode: number) {
+    this.title = title;
+    this.priceCode = priceCode;
+  }
 }
 
 export const MOVIE_CATEGORY = {
-  CHILDRENS: 2,
+  CHILDREN: 2,
   REGULAR: 0,
-  NEW_RELEASE: 1
+  NEW_RELEASE: 1,
 };
 
 type RENTAL = {
-  movie: Movie,
-  number: number
-}
+  movie: Movie;
+  number: number;
+};
 export class Customer {
   private rentals: RENTAL[] = [];
 
   constructor(private readonly name: string) {}
 
   public addRental(m: Movie, d: number) {
-    this.rentals.push({number: d, movie: m});
+    this.rentals.push({ number: d, movie: m });
   }
 
   public statement(): string {
@@ -27,36 +31,36 @@ export class Customer {
     let frequentRenterPoints = 0;
 
     let result = "Rental Record for " + this.name + "\n";
-    for (const r of this.rentals) {
-      let each = r.movie;
-      let thisAmount = 0;
-      let dr = r.number;
+    for (const rental of this.rentals) {
+      let movie = rental.movie; //let
+      let moviePrice = 0;
+      let basePrice = rental.number;
       // determine amounts for each line
-      switch (each.priceCode) {
+      switch (movie.priceCode) {
         case MOVIE_CATEGORY.REGULAR:
-          thisAmount += 2;
-          if (dr > 2)
-            thisAmount += (dr - 2) * 1.5; // magic number
+          moviePrice += 2;
+          if (basePrice > 2) moviePrice += (basePrice - 2) * 1.5; // magic number
           break;
         case MOVIE_CATEGORY.NEW_RELEASE:
-          thisAmount += dr * 3; // magic number
+          moviePrice += basePrice * 3; // magic number
           break;
-        case MOVIE_CATEGORY.CHILDRENS:
-          thisAmount += 1.5; // magic number
-          if (dr > 3)
-            thisAmount += (dr - 3) * 1.5; // magic number
+        case MOVIE_CATEGORY.CHILDREN:
+          moviePrice += 1.5; // magic number
+          if (basePrice > 3) moviePrice += (basePrice - 3) * 1.5; // magic number
           break;
       }
       // add frequent renter points
       frequentRenterPoints++;
       // add bonus for a two day new release rental
-      if (each.priceCode != null &&
-          (each.priceCode == MOVIE_CATEGORY.NEW_RELEASE)
-          && dr > 1)
+      if (
+        movie.priceCode != null &&
+        movie.priceCode == MOVIE_CATEGORY.NEW_RELEASE &&
+        basePrice > 1
+      )
         frequentRenterPoints++;
       // show figures line for this rental
-      result += "\t" + each.title + "\t" + thisAmount.toFixed(1) + "\n";
-      totalAmount += thisAmount;
+      result += "\t" + movie.title + "\t" + moviePrice.toFixed(1) + "\n";
+      totalAmount += moviePrice;
     }
     // add footer lines
     result += "Amount owed is " + totalAmount.toFixed(1) + "\n";
