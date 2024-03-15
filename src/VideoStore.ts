77f4b64cf60020c3c6aa0cbbe4cb918749c6ee57
sterlu <sterlu.sd@gmail.com>
2024-03-15 15:00:02 +0100

    Refactor Rental to class

--- src/VideoStore.ts
+++ src/VideoStore.ts
@@ -7,8 +7,6 @@ export class Movie {
   }
 }
 
-type Rental = { movie: Movie, daysRenting: number };
-
 export enum MovieCategory {
   REGULAR = 'REGULAR',
   NEW_RELEASE = 'NEW_RELEASE',
@@ -21,6 +19,33 @@ const moviePricing:Record<MovieCategory, {basePrice:number,dayRentingCutoff:numb
     [MovieCategory.CHILDRENS]: {basePrice: 1.5, extraMultiplier: 1.5, dayRentingCutoff: 3},
 }
 
+class Rental {
+  public movie: Movie;
+  public daysRenting: number;
+  constructor(movie: Movie, daysRenting: number) {
+    this.movie = movie;
+    this.daysRenting = daysRenting;
+  }
+
+  public getPrice(): number {
+    const {
+      basePrice, dayRentingCutoff, extraMultiplier
+    } = moviePricing[this.movie.movieCategory];
+
+    let price = basePrice;
+    if(this.daysRenting > dayRentingCutoff){
+      price += (this.daysRenting - dayRentingCutoff) * extraMultiplier;
+    }
+
+    return price;
+  }
+
+  public getFrequentRenterPoints(): number {
+    if(this.movie.movieCategory === MovieCategory.NEW_RELEASE && this.daysRenting > 1) return 2;
+    return 1;
+  }
+}
+
 export class Customer {
   private name: string;
   private rentals: Rental[] = [];
@@ -30,39 +55,17 @@ export class Customer {
   }
 
   public addRental(movie: Movie, daysRenting: number) {
-    this.rentals.push({ movie, daysRenting });
+    this.rentals.push(new Rental(movie, daysRenting));
   }
-
-  private calculateMoviePrice(rental: Rental): number {
-    const { movie, daysRenting } = rental;
-    const {basePrice,dayRentingCutoff,extraMultiplier} = moviePricing[movie.movieCategory];
-
-    let price = basePrice;
-    if(daysRenting > dayRentingCutoff){
-        price += (daysRenting - dayRentingCutoff) * extraMultiplier;
-    }
-
-    return price;
-  }
-
-  private getFrequentRenterPointsForRental(rental: Rental): number {
-    const { movie, daysRenting } = rental;
-    if(movie.movieCategory === MovieCategory.NEW_RELEASE && daysRenting > 1) return 2;
-    return 1;
-  }
-
   public statement(): string {
     let totalPrice: number = 0;
     let frequentRenterPoints = 0;
 
     let result = "Rental Record for " + this.name + "\n";
     for (const rental of this.rentals) {
-      const { movie } = rental;
-
-      const price = this.calculateMoviePrice(rental);
-      frequentRenterPoints+= this.getFrequentRenterPointsForRental(rental);
-
-      result += "\t" + movie.title + "\t" + price.toFixed(1) + "\n";
+      const price = rental.getPrice();
+      frequentRenterPoints += rental.getFrequentRenterPoints();
+      result += "\t" + rental.movie.title + "\t" + price.toFixed(1) + "\n";
       totalPrice += price;
     }
     // add footer lines
