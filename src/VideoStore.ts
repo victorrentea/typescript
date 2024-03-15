6eb74eea4160a8c5f077303e31159ab37f1ec951
MatijaPojatar <58627531+MatijaPojatar@users.noreply.github.com>
2024-03-15 14:38:47 +0100

    Move switch to separate function

--- src/VideoStore.ts
+++ src/VideoStore.ts
@@ -7,6 +7,8 @@ export class Movie {
   }
 }
 
+type Rental = { movie: Movie, daysRenting: number };
+
 export enum MovieCategory {
   REGULAR= 0,
   NEW_RELEASE= 1,
@@ -16,54 +18,57 @@ export enum MovieCategory {
 
 export class Customer {
   private name: string;
-  private rentals: {movie:Movie,d:number}[] = [];
+  private rentals: Rental[] = [];
 
   constructor(name: string) {
     this.name = name;
   }
 
-  public addRental(movie: Movie, d: number) {
-    this.rentals.push({ movie, d });
+  public addRental(movie: Movie, daysRenting: number) {
+    this.rentals.push({ movie, daysRenting });
+  }
+
+  private calculateMoviePrice(rental: Rental): number {
+    const { movie, daysRenting } = rental;
+    let price = 0;
+    switch (movie.movieCategory) {
+      case MovieCategory.REGULAR:
+        price = 2;
+        if (daysRenting > 2)
+          price += (daysRenting - 2) * 1.5;
+        break;
+      case MovieCategory.NEW_RELEASE:
+        price = daysRenting * 3;
+        break;
+      case MovieCategory.CHILDRENS:
+        price = 1.5;
+        if (daysRenting > 3)
+          price += (daysRenting - 3) * 1.5;
+        break;
+    }
+    return price;
   }
 
   public statement(): string {
-    let totalAmount: number = 0;
+    let totalPrice: number = 0;
     let frequentRenterPoints = 0;
 
     let result = "Rental Record for " + this.name + "\n";
-    for (const r of this.rentals) {
-      let each = r.movie;
-      let thisAmount = 0;
-      let dr = r.d;
+    for (const rental of this.rentals) {
       // determine amounts for each line
-      switch (each.movieCategory) {
-        case MovieCategory.REGULAR:
-          thisAmount += 2;
-          if (dr > 2)
-            thisAmount += (dr - 2) * 1.5;
-          break;
-        case MovieCategory.NEW_RELEASE:
-          thisAmount += dr * 3;
-          break;
-        case MovieCategory.CHILDRENS:
-          thisAmount += 1.5;
-          if (dr > 3)
-            thisAmount += (dr - 3) * 1.5;
-          break;
-      }
+      const price = this.calculateMoviePrice(rental);
+      const { daysRenting, movie } = rental;
       // add frequent renter points
       frequentRenterPoints++;
       // add bonus for a two day new release rental
-      if (each.movieCategory != null &&
-          (each.movieCategory == MovieCategory.NEW_RELEASE)
-          && dr > 1)
+      if (movie.movieCategory === MovieCategory.NEW_RELEASE && daysRenting > 1)
         frequentRenterPoints++;
       // show figures line for this rental
-      result += "\t" + each.title + "\t" + thisAmount.toFixed(1) + "\n";
-      totalAmount += thisAmount;
+      result += "\t" + movie.title + "\t" + price.toFixed(1) + "\n";
+      totalPrice += price;
     }
     // add footer lines
-    result += "Amount owed is " + totalAmount.toFixed(1) + "\n";
+    result += "Amount owed is " + totalPrice.toFixed(1) + "\n";
     result += "You earned " + frequentRenterPoints + " frequent renter points";
     return result;
   }
