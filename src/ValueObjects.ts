114ece69ce61e6984d755a86db379925fb1b32f7
Victor Rentea <victorrentea@gmail.com>
2024-08-27 10:51:32 +0300

    discovered a new concept: Interval type, used it in my signature to reduce the number of params

--- src/ValueObjects.ts
+++ src/ValueObjects.ts
@@ -1,52 +1,53 @@
 import {expect} from "chai";
 
-
 function filterCarModels(criteria: CarSearchCriteria, models: CarModel[]): CarModel[] {
 // new collection, less noise , FP ftw!
-    return models.filter(model => model.intersectsCriteriaYears(criteria));
+  return models.filter(model =>
+      MathUtil.intervalsIntersect2({start: model.startYear, end: model.endYear}, {
+        start: criteria.startYear,
+        end: criteria.endYear
+      }));
 }
 
-function applyCapacityFilter() {
-    console.log(MathUtil.intervalsIntersect(1000, 1600, 1250, 2000));
-}
 
 class MathUtil {
+  // more semantics but harder to call
+  static intervalsIntersect2(interval1: Interval, interval2: Interval): boolean {
+    return interval1.start <= interval2.end && interval2.start <= interval1.end;
+  }
+}
 
-
-    static intervalsIntersect(start1: number, end1: number, start2: number, end2: number): boolean {
-        return start1 <= end2 && start2 <= end1;
-    }
+// type Interval = [number, number];
+interface Interval {
+  start: number;
+  end: number;
 }
 
 
 class CarSearchCriteria { // comes from JSON
-    constructor(public readonly startYear: number,
-                public readonly endYear: number,
-                public readonly make: string) {
-        if (startYear > endYear) throw new Error("start larger than end");
-    }
+  constructor(public readonly startYear: number,
+              public readonly endYear: number,
+              public readonly make: string) {
+    if (startYear > endYear) throw new Error("start larger than end");
+  }
 }
 
 class CarModel { // from my private DB = Domain Model
-    constructor(public readonly make: string,
-                public readonly model: string,
-                public readonly startYear: number,
-                public readonly endYear: number) {
-        if (startYear > endYear) {
-            throw new Error("start larger than end");
-        }
+  constructor(public readonly make: string,
+              public readonly model: string,
+              public readonly startYear: number,
+              public readonly endYear: number) {
+    if (startYear > endYear) {
+      throw new Error("start larger than end");
     }
-
-    intersectsCriteriaYears(criteria: CarSearchCriteria) {
-        return MathUtil.intervalsIntersect(this.startYear, this.endYear, criteria.startYear, criteria.endYear);
-    }
+  }
 }
 
 it('should filter car models', () => {
-    const criteria = new CarSearchCriteria(2014, 2018, "Ford");
-    const fordFocusMk2 = new CarModel("Ford", "Focus", 2012, 2016);
-    const models = filterCarModels(criteria, [fordFocusMk2]);
-    console.log(models);
-    expect(models).to.contain(fordFocusMk2);
+  const criteria = new CarSearchCriteria(2014, 2018, "Ford");
+  const fordFocusMk2 = new CarModel("Ford", "Focus", 2012, 2016);
+  const models = filterCarModels(criteria, [fordFocusMk2]);
+  console.log(models);
+  expect(models).to.contain(fordFocusMk2);
 });
 
