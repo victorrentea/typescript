6b9a6b70fb2161f28b3d907c32d8b14887fe424e
Victor Rentea <victorrentea@gmail.com>
2024-08-27 10:55:49 +0300

    OOP on the new concept i created a class for

--- src/ValueObjects.ts
+++ src/ValueObjects.ts
@@ -2,50 +2,50 @@ import {expect} from "chai";
 
 function filterCarModels(criteria: CarSearchCriteria, models: CarModel[]): CarModel[] {
 // new collection, less noise , FP ftw!
-  return models.filter(model =>
-      new Interval(model.startYear, model.endYear)
-          .intersect(new Interval(criteria.startYear, criteria.endYear)
-          ));
+    return models.filter(model =>
+        new Interval(model.startYear, model.endYear)
+            .intersect(
+                new Interval(criteria.startYear, criteria.endYear)
+            ));
 }
-
 
 
 // type Interval = [number, number];
 class Interval {
-  constructor(public readonly start: number, public readonly end: number) {
-    if (start > end) throw new Error("start larger than end");
-  }
+    constructor(public readonly start: number, public readonly end: number) {
+        if (start > end) throw new Error("start larger than end");
+    }
 
-  intersect(other: Interval): boolean {
-    return this.start <= other.end && other.start <= this.end;
-  }
+    intersect(other: Interval): boolean {
+        return this.start <= other.end && other.start <= this.end;
+    }
 }
 
 
 class CarSearchCriteria { // comes from JSON
-  constructor(public readonly startYear: number,
-              public readonly endYear: number,
-              public readonly make: string) {
-    if (startYear > endYear) throw new Error("start larger than end");
-  }
+    constructor(public readonly startYear: number,
+                public readonly endYear: number,
+                public readonly make: string) {
+        if (startYear > endYear) throw new Error("start larger than end");
+    }
 }
 
 class CarModel { // from my private DB = Domain Model
-  constructor(public readonly make: string,
-              public readonly model: string,
-              public readonly startYear: number,
-              public readonly endYear: number) {
-    if (startYear > endYear) {
-      throw new Error("start larger than end");
+    constructor(public readonly make: string,
+                public readonly model: string,
+                public readonly startYear: number,
+                public readonly endYear: number) {
+        if (startYear > endYear) {
+            throw new Error("start larger than end");
+        }
     }
-  }
 }
 
 it('should filter car models', () => {
-  const criteria = new CarSearchCriteria(2014, 2018, "Ford");
-  const fordFocusMk2 = new CarModel("Ford", "Focus", 2012, 2016);
-  const models = filterCarModels(criteria, [fordFocusMk2]);
-  console.log(models);
-  expect(models).to.contain(fordFocusMk2);
+    const criteria = new CarSearchCriteria(2014, 2018, "Ford");
+    const fordFocusMk2 = new CarModel("Ford", "Focus", 2012, 2016);
+    const models = filterCarModels(criteria, [fordFocusMk2]);
+    console.log(models);
+    expect(models).to.contain(fordFocusMk2);
 });
 
