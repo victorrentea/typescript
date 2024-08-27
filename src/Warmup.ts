825d46a593b255799cdc5e073117deb2fd2eb29d
Victor Rentea <victorrentea@gmail.com>
2024-08-27 09:20:38 +0300

    added warmup

---
+++ src/Warmup.ts
@@ -0,0 +1,50 @@
+interface R {
+    x: number;
+}
+
+class One {
+    private two: Two;
+
+    constructor(two: Two) {
+        this.two = two;
+    }
+
+    f(): number {
+        return 2 * this.two.g({x: 3});
+    }
+}
+
+class Two {
+    g(r: R): number {
+        const b = 2;
+        console.log("b=" + b);
+        return 1 + b + r.x;
+    }
+
+    unknown(): void {
+        console.log("b=" + 987);
+    }
+}
+
+
+// TODO: Practice Refactoring
+//  * How to?
+//    - Right-click > Refactor
+//    - Ctrl-Alt-Shift-T/^T to
+//    - Keys: [Ctrl-Alt / Opt-Cmd] + [V]ariable/[M]ethod/[P]arameter/i[N]line
+//  * What? // after every action undo/revert to start clean
+//    - Inline[N] Variable 'b'
+//    - Extract [V]ariable '1', '3 * two.g()'
+//    - Extract [M]ethod 'System.out..'
+//    - Inline[N] Method 'g'
+//    - Extract [P]arameter '1', 'r.x()'
+//    - Inline[N] Parameter 'c'
+//    - Change Signature 'g': add 1 param with default as 1st arg
+//    - Extract Interface 'Two'->ITwo; - Inline to Anonymous Class to destroy interface
+//    - Rename 'g' -> 'h' by Shift-F6 or just edit>Alt-Enter>Rename
+//    - Move Method 'g' into R
+//    - Preview method/class: Ctrl-Shift-I
+//    - Quickfix for->stream
+//    - Change inspection severity & highlighting
+//       * Download "aggressive_refactoring.xml" from https://victorrentea.ro
+//       and import it in Settings>Editor>Inspections
