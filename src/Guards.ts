ff7d33f9032c8e44e8b6dfc8de06b3590aa2153b
Victor Rentea <victorrentea@gmail.com>
2024-08-28 10:03:54 +0300

    guad conditions

--- src/Guards.ts
+++ src/Guards.ts
@@ -1,28 +1,26 @@
 function getPayAmount(marine: Marine, bonusPackage: BonusPackage): number {
-    let result: number;
-    if (marine != null && (bonusPackage.value > 100 || bonusPackage.value < 10)) {
-        if (!retrieveDeadStatus(marine)) {
-            if (!marine.retired) {
-                if (marine.yearsService != null) {
-                    result = marine.yearsService * 100 + bonusPackage.value;
-                    if (marine.awards && marine.awards.length !== 0) {
-                        result += 1000;
-                    }
-                    if (marine.awards && marine.awards.length >= 3) {
-                        result += 2000;
-                    }
-                    // HEAVY core logic here, business-rules ...
-                } else {
-                    throw new Error("Any marine should have the years of service set");
-                }
-            } else result = retiredAmount();
-            // this!!
-        } else {
-            result = deadAmount();
-        }
-    } else {
+    if (marine == null)
         throw new Error("Not applicable!");
+    if (bonusPackage.value <= 100 && bonusPackage.value >= 10)
+        throw new Error("Not applicable!");
+    if (retrieveDeadStatus(marine)) {
+        return deadAmount();
     }
+    if (marine.retired) {
+        return retiredAmount();
+    }
+    if (marine.yearsService == null) {
+        throw new Error("Any marine should have the years of service set");
+    }
+    let result: number;
+    result = marine.yearsService * 100 + bonusPackage.value;
+    if (marine.awards && marine.awards.length !== 0) {
+        result += 1000;
+    }
+    if (marine.awards && marine.awards.length >= 3) {
+        result += 2000;
+    }
+    // HEAVY core logic here, business-rules ...
     return result;
 }
 
--- src/fp/Pure.ts
+++ src/fp/Pure.ts
@@ -66,18 +66,58 @@ class Pure {
     ) {
     }
 
+    // CouponApplier (NO:PriceHelper,or Util) class when it grows.
+    static applyCoupons(products: Product[], resolvedPrices: Map<number, number>, customer: Customer) {
+        // should I change the prod design just for testing?
+        // improving = yes
+        // breaking = no
+        // ?? is making this method a big mistake (breaking encapsulation) private -> public.
+        //     YES, it's a mistake => extract this method to a separate class
+        //     NO imho
+        const usedCoupons: Coupon[] = []; // accumulator variables
+        const finalPrices = new Map<number, number>();
+        for (const product of products) {
+            let price = resolvedPrices.get(product.id) as number;
+            // TODO 2024-08-28 vrentea - uncomment this code after PO replies to email sent on 28 aug
+            // if (!price) { //after confirming with the PO. if PO is in holiday, add this code commeted out, send her an email
+            //     throw new Error("Could not find price for product: " + product.id);
+            // }
+            for (const coupon of customer.coupons()) {
+                if (coupon.autoApply && coupon.isApplicableFor(product) && !usedCoupons.includes(coupon)) {
+                    price = coupon.apply(product, price);
+                    usedCoupons.push(coupon);
+                }
+            }
+            finalPrices.set(product.id, price);
+        }
+        return {usedCoupons, finalPrices};
+    }
+
+    // this is a pure function. Therefore it is NOT async, and it does use any dependencies, nor MOCK in test.
+    // yee haa! tests without mocks. with subcutaneous tests.
+    // i made it static to EXPRESS the fact it's pure. static method cannot use injected dependencies. Only parameters.
+    // don't put in a Util. it's very specific to my flow. leave it here. Maybe move to a
+
     // TODO extract a pure function with as much logic possible
     async computePrices(customerId: number,
                         productIds: number[],
                         internalPrices: Map<number, number>
     ): Promise<Map<number, number>> {
 
-        //aplied split phase refactor
-        // phase 1: fetch data
         const customer = await this.customerApi.findById(customerId); // IO: GET/SELECT .. WHERE ID = ?
         const products = await this.productApi.findAllById(productIds);  // GET many?id=1,2,3/SELECT .. WHERE ID IN (?, ?, ?)
 
-        // phase 2: resolve prices
+        const resolvedPrices = await this.resolvePrices(products, internalPrices);
+
+        // not desireable to return mutliple values from a single function
+        const {usedCoupons, finalPrices} = Pure.applyCoupons(products, resolvedPrices, customer);
+        // const {usedCoupons, finalPrices} = Pure.applyCoupons(products, resolvedPrices, customer);
+
+        await this.couponRepo.markUsedCoupons(customerId, usedCoupons); // INSERT // side effect
+        return finalPrices;
+    }
+
+    private async resolvePrices(products: Product[], internalPrices: Map<number, number>) {
         const resolvedPrices = new Map<number, number>();
         for (const product of products) {
             let price: number | undefined = internalPrices.get(product.id);
@@ -88,23 +128,6 @@ class Pure {
             // finalPrices.set(product.id, price); // BAD: because the map values now carry different meanings in different parts of the code
             resolvedPrices.set(product.id, price);
         }
-
-        // phase 3: apply coupons
-        const usedCoupons: Coupon[] = []; // accumulator variables
-        const finalPrices = new Map<number, number>();
-        for (const product of products) {
-            let price = resolvedPrices.get(product.id);
-            for (const coupon of customer.coupons()) {
-                if (coupon.autoApply && coupon.isApplicableFor(product) && !usedCoupons.includes(coupon)) {
-                    price = coupon.apply(product, price);
-                    usedCoupons.push(coupon);
-                }
-            }
-            finalPrices.set(product.id, price);
-        }
-
-        // phase 4: side effects
-        await this.couponRepo.markUsedCoupons(customerId, usedCoupons); // INSERT // side effect
-        return finalPrices;
+        return resolvedPrices;
     }
 }
