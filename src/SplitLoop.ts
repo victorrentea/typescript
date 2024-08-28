e742b01f4fcc693c4d88211867c1d6cf91977c6d
Victor Rentea <victorrentea@gmail.com>
2024-08-28 10:43:28 +0300

    replaced loop with FP pipeline

--- src/SplitLoop.ts
+++ src/SplitLoop.ts
@@ -8,13 +8,27 @@ export type Employee = {
 
 
 export function computeStatsHard(employees: Employee[], retrieveSalary: (employeeId: number) => number | null): string {
-    let totalEmpAge: number = 0;
     const consultantsPaidOver3K: Employee[] = [];
+
     for (const employee of employees) {
         if (employee.isConsultant) {
             if (employee.id == null) {
                 return "Employee(s) not persisted";
             }
+        }
+    }
+
+
+    const totalEmpAge = employees.filter(e => !e.isConsultant)
+        .reduce((total, e) => total + e.age, 0); // A
+
+    // .map(e => e.age)
+    // .reduce((a, b) => a + b, 0); // B
+    // .reduce(sum, 0); // B
+
+
+    for (const employee of employees) {
+        if (employee.isConsultant) {
             if (employee.salary == null) {
                 const salary = retrieveSalary(employee.id);
                 if (salary == null) {
@@ -27,11 +41,12 @@ export function computeStatsHard(employees: Employee[], retrieveSalary: (employe
                 consultantsPaidOver3K.push(employee);
             }
         }
-
-        if (!employee.isConsultant) {
-            totalEmpAge += employee.age;
-        }
     }
+
+    // debate: performance: how many elements are in the employees list?
+    // 100 -> not a problem
+    // 1M -> problem. BUT! where did that 1M come from? DB/API?
+    //    then bringing in that amount of data took an eternity. relative impact of my extra for = 0.0000001
 
     let averageEmpAge: number = 0;
     if (totalEmpAge != 0) {
