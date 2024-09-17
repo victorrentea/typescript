9e9b3001c22766702da3a34d2cb24c183cf12b16
idaa <ida.amit@gmail.com>
2024-09-17 14:58:21 +0300

    first commit

--- src/VideoStore.ts
+++ src/VideoStore.ts
@@ -1,66 +1,81 @@
-export class Movie {
-  public title: string;
-  public priceCode: number;
-}
+type Movie = {
+    title: string;
+    priceCode: number;
+};
+
+type Rental = {
+    days: number;
+    movie: Movie;
+};
 
 export const MOVIE_CATEGORY = {
-  CHILDRENS: 2,
-  REGULAR: 0,
-  NEW_RELEASE: 1
+    CHILDRENS: 2,
+    REGULAR: 0,
+    NEW_RELEASE: 1
 };
 
 
 export class Customer {
-  private name: string;
-  private rentals: any[] = [];
+    private name: string;
+    private rentals: Rental[] = [];
 
-  constructor(name: string) {
-    this.name = name;
-  }
-
-  public addRental(m: Movie, d: number) {
-    this.rentals.push({d: d, m: m});
-  }
-
-  public statement(): string {
-    let totalAmount: number = 0;
-    let frequentRenterPoints = 0;
-
-    let result = "Rental Record for " + this.name + "\n";
-    for (const r of this.rentals) {
-      let each = r.m;
-      let thisAmount = 0;
-      let dr = r.d;
-      // determine amounts for each line
-      switch (each.priceCode) {
-        case MOVIE_CATEGORY.REGULAR:
-          thisAmount += 2;
-          if (dr > 2)
-            thisAmount += (dr - 2) * 1.5;
-          break;
-        case MOVIE_CATEGORY.NEW_RELEASE:
-          thisAmount += dr * 3;
-          break;
-        case MOVIE_CATEGORY.CHILDRENS:
-          thisAmount += 1.5;
-          if (dr > 3)
-            thisAmount += (dr - 3) * 1.5;
-          break;
-      }
-      // add frequent renter points
-      frequentRenterPoints++;
-      // add bonus for a two day new release rental
-      if (each.priceCode != null &&
-          (each.priceCode == MOVIE_CATEGORY.NEW_RELEASE)
-          && dr > 1)
-        frequentRenterPoints++;
-      // show figures line for this rental
-      result += "\t" + each.title + "\t" + thisAmount.toFixed(1) + "\n";
-      totalAmount += thisAmount;
+    constructor(name: string) {
+        this.name = name;
     }
-    // add footer lines
-    result += "Amount owed is " + totalAmount.toFixed(1) + "\n";
-    result += "You earned " + frequentRenterPoints + " frequent renter points";
-    return result;
-  }
+
+    public addRental(movie: Movie, days: number) {
+        this.rentals.push({days: days, movie: movie});
+    }
+
+    public statement(): string {
+        let totalAmount: number = 0;
+        let frequentRenterPoints = 0;
+
+        let result = "Rental Record for " + this.name + "\n";
+        for (const rental of this.rentals) {
+            const currentMovie = rental.movie;
+            const numberOfRentedDays = rental.days;
+            // determine amounts for each line
+            const moviePrice = this.calculatePrice(currentMovie, numberOfRentedDays);
+            // add frequent renter points
+            frequentRenterPoints++;
+            // add bonus for a two day new release rental
+            frequentRenterPoints = this.AddBonus(currentMovie, numberOfRentedDays, frequentRenterPoints);
+            // show figures line for this rental
+            result += "\t" + currentMovie.title + "\t" + moviePrice.toFixed(1) + "\n";
+            totalAmount += moviePrice;
+        }
+        // add footer lines
+        result += "Amount owed is " + totalAmount.toFixed(1) + "\n";
+        result += "You earned " + frequentRenterPoints + " frequent renter points";
+        return result;
+    }
+
+    private AddBonus(currentMovie: Movie, numberOfRentedDays: number, frequentRenterPoints: number) {
+        if (currentMovie.priceCode == MOVIE_CATEGORY.NEW_RELEASE
+            && numberOfRentedDays > 1) {
+            frequentRenterPoints++;
+        }
+        return frequentRenterPoints;
+    }
+
+    private calculatePrice(currentMovie: Movie, numberOfRentedDays: number) {
+        let moviePrice = 0;
+        switch (currentMovie.priceCode) {
+            case MOVIE_CATEGORY.REGULAR:
+                moviePrice += 2;
+                if (numberOfRentedDays > 2)
+                    moviePrice += (numberOfRentedDays - 2) * 1.5;
+                break;
+            case MOVIE_CATEGORY.NEW_RELEASE:
+                moviePrice += numberOfRentedDays * 3;
+                break;
+            case MOVIE_CATEGORY.CHILDRENS:
+                moviePrice += 1.5;
+                if (numberOfRentedDays > 3)
+                    moviePrice += (numberOfRentedDays - 3) * 1.5;
+                break;
+        }
+        return moviePrice;
+    }
 }
