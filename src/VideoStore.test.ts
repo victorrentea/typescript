01d27144db2fe3718a54d0ab8fd58147cc259422
joeg <joe.gilad@nice.com>
2024-09-17 15:03:58 +0300

    continue refactoring - main statement

--- src/VideoStore.test.ts
+++ src/VideoStore.test.ts
@@ -3,15 +3,16 @@ import {expect} from 'chai';
 import {describe, it} from 'mocha';
 import {MOVIE_CATEGORY} from "./VideoStore";
 import {Customer} from "./entities/customer";
+import {Rental} from "./entities/rental";
 
 
 describe('Videostore', () => {
     it('Characterization Test ', function () {
 
     const customer = new Customer("John Doe");
-    customer.addRental({title:"Star Wars", priceCode: MOVIE_CATEGORY.NEW_RELEASE}, 6);
-    customer.addRental({title:"Sofia", priceCode: MOVIE_CATEGORY.CHILDREN}, 7);
-    customer.addRental({title:"Inception", priceCode: MOVIE_CATEGORY.REGULAR}, 5);
+    customer.addRental(new Rental({title:"Star Wars", priceCode: MOVIE_CATEGORY.NEW_RELEASE}, 6));
+    customer.addRental(new Rental({title:"Sofia", priceCode: MOVIE_CATEGORY.CHILDREN}, 7));
+    customer.addRental(new Rental({title:"Inception", priceCode: MOVIE_CATEGORY.REGULAR}, 5));
 
     const expected = "Rental Record for John Doe\n"
       + "	Star Wars	18.0\n"
--- src/entities/customer.ts
+++ src/entities/customer.ts
@@ -1,6 +1,6 @@
-import {Movie} from "./movie";
 import {MOVIE_CATEGORY} from "../VideoStore";
 import {Rental} from "./rental";
+import {Movie} from "./movie";
 
 export class Customer {
     private rentals: Rental[] = [];
@@ -18,40 +18,42 @@ export class Customer {
         let frequentRenterPoints = 0;
 
         let result = "Rental Record for " + this.name + "\n";
-        for (const r of this.rentals) {
-            let each = r.movie;
+        for (const rental of this.rentals) {
             let thisAmount = 0;
-            let dr = r.d;
             // determine amounts for each line
-            switch (each.priceCode) {
-                case MOVIE_CATEGORY.REGULAR:
-                    thisAmount += 2;
-                    if (dr > 2)
-                        thisAmount += (dr - 2) * 1.5;
-                    break;
-                case MOVIE_CATEGORY.NEW_RELEASE:
-                    thisAmount += dr * 3;
-                    break;
-                case MOVIE_CATEGORY.CHILDREN:
-                    thisAmount += 1.5;
-                    if (dr > 3)
-                        thisAmount += (dr - 3) * 1.5;
-                    break;
-            }
+            thisAmount = this.calculateRentPrice(rental.movie, thisAmount, rental);
             // add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two day new release rental
-            if (each.priceCode != null &&
-                (each.priceCode == MOVIE_CATEGORY.NEW_RELEASE)
-                && dr > 1)
+            if (rental.movie.priceCode == MOVIE_CATEGORY.NEW_RELEASE
+                && rental.daysRent > 1)
                 frequentRenterPoints++;
             // show figures line for this rental
-            result += "\t" + each.title + "\t" + thisAmount.toFixed(1) + "\n";
+            result += "\t" + rental.movie.title + "\t" + thisAmount.toFixed(1) + "\n";
             totalAmount += thisAmount;
         }
         // add footer lines
         result += "Amount owed is " + totalAmount.toFixed(1) + "\n";
         result += "You earned " + frequentRenterPoints + " frequent renter points";
         return result;
+    }
+
+    private calculateRentPrice(movie: Movie, thisAmount: number, rental: Rental) {
+        switch (movie.priceCode) {
+            case MOVIE_CATEGORY.REGULAR:
+                thisAmount += 2;
+                if (rental.daysRent > 2)
+                    thisAmount += (rental.daysRent - 2) * 1.5;
+                break;
+            case MOVIE_CATEGORY.NEW_RELEASE:
+                thisAmount += rental.daysRent * 3;
+                break;
+            case MOVIE_CATEGORY.CHILDREN:
+                thisAmount += 1.5;
+                if (rental.daysRent > 3)
+                    thisAmount += (rental.daysRent - 3) * 1.5;
+                break;
+        }
+        return thisAmount;
     }
 }
