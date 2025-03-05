7f7b18b6bb0f965a66f22513075edb8685067899
Victor Rentea <victorrentea@gmail.com>
2025-03-05 10:41:56 +0200

    done

--- src/Guards.ts
+++ src/Guards.ts
@@ -1,50 +1,50 @@
+// @Valid
 function getPayAmount(marine: Marine, bonusPackage: BonusPackage): number {
-    let result: number;
-    if (marine != null && (bonusPackage.value > 100 || bonusPackage.value < 10)) {
-        if (!retrieveDeadStatus(marine)) {
-            if (!marine.retired) {
-                if (marine.yearsService != null) {
-                    result = marine.yearsService * 100 + bonusPackage.value;
-                    if (marine.awards && marine.awards.length !== 0) {
-                        result += 1000;
-                    }
-                    if (marine.awards && marine.awards.length >= 3) {
-                        result += 2000;
-                    }
-                    // HEAVY core logic here, business-rules ...
-                } else {
-                    throw new Error("Any marine should have the years of service set");
-                }
-            } else result = retiredAmount();
-            // this!!
-        } else {
-            result = deadAmount();
-        }
-    } else {
-        throw new Error("Not applicable!");
-    }
-    return result;
+  const applicable = marine != null && (bonusPackage.value > 100 || bonusPackage.value < 10);
+  if (!applicable)
+    throw new Error("Not applicable!");
+
+  if (!retrieveDeadStatus(marine)) {
+    return deadAmount();
+  }
+  if (marine.retired) {
+    return retiredAmount();
+  }
+  if (marine.yearsService == null) {
+    throw new Error("Any marine should have the years of service set");
+  }
+  let result: number = marine.yearsService * 100 + bonusPackage.value;
+  if (marine.awards && marine.awards.length !== 0) {
+    result += 1000;
+  }
+  if (marine.awards && marine.awards.length >= 3) {
+    result += 2000;
+  }
+  // HEAVY core logic here, business-rules ...
+
+
+  return result;
 }
 
 function retrieveDeadStatus(marine: Marine): boolean {
-    // after 500 millis
-    return false;
+  // after 500 millis
+  return false;
 }
 
 function deadAmount(): number {
-    return 1;
+  return 1;
 }
 
 function retiredAmount(): number {
-    return 2;
+  return 2;
 }
 
 
 type Marine = {
-    dead: boolean,
-    retired: boolean,
-    awards: Award[],
-    yearsService?: number
+  dead: boolean,
+  retired: boolean,
+  awards: Award[],
+  yearsService?: number
 }
 type BonusPackage = { value: number }
 
--- src/SplitLoop.ts
+++ src/SplitLoop.ts
@@ -1,41 +1,26 @@
 export type Employee = {
-    id: number | null;
-    name: string | null;
-    age: number;
-    isConsultant: boolean;
-    salary: number | null;
+  id: number | null;
+  name: string | null;
+  age: number;
+  isConsultant: boolean;
+  salary: number;
 };
 
 
 export function computeStatsHard(employees: Employee[], retrieveSalary: (employeeId: number) => number | null): string {
-    let totalEmpAge: number = 0;
-    const consultantsPaidOver3K: Employee[] = [];
-    for (const employee of employees) {
-        if (!employee.isConsultant) {
-            totalEmpAge += employee.age;
-            continue;
-        }
-        if (employee.id == null) {
-            return "Employee(s) not persisted";
-        }
-        if (employee.salary == null) {
-            const salary = retrieveSalary(employee.id);
-            if (salary == null) {
-                throw new Error("NO salary found for employee " + employee.id);
-            } else {
-                employee.salary = salary;
-            }
-        }
-        if (employee.salary > 3000) {
-            consultantsPaidOver3K.push(employee);
-        }
-    }
+  const totalEmpAge = employees
+    .filter(e => !e.isConsultant)
+    .map(e => e.age)
+    .reduce((a, b) => a + b);
 
-    let averageEmpAge: number = 0;
-    if (totalEmpAge != 0) {
-        averageEmpAge = totalEmpAge / employees.filter(e => !e.isConsultant).length;
-    }
-    console.log("some logic with ", consultantsPaidOver3K);
-    return "Average employee age = " + averageEmpAge;
+  const consultantsPaidOver3K = employees.filter(e => e.isConsultant && e.salary > 3000);
+
+
+  let averageEmpAge: number = 0;
+  if (totalEmpAge != 0) {
+    averageEmpAge = totalEmpAge / employees.filter(e => !e.isConsultant).length;
+  }
+  console.log("some logic with ", consultantsPaidOver3K);
+  return "Average employee age = " + averageEmpAge;
 }
 
